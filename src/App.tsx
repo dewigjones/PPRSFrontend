48d2c8c9473a07b49c1d1bef64aadaba7914e823
dewigjones <87873790+dewigjones@users.noreply.github.com>
2024-02-18 17:48:30 +0000

    API call test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import MovieBar from './MovieBar'
 
 function App() {
 const apikey = import.meta.env.VITE_REACT_APP_TMDB_API_KEY;
+const FeaturedApi = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apikey}&page=1`;
+const getMovies = (API: string) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+      });
+  };
+
+ getMovies(FeaturedApi); 
   return (
     <>
       <h1>Privacy Preserving Recommender System</h1>
