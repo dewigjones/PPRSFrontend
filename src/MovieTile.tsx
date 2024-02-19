bc15233b81b7c20e8f064af5305395efcd6991af
dewigjones <87873790+dewigjones@users.noreply.github.com>
2024-02-19 13:48:55 +0000

    Scrollable without bar

--- src/MovieTile.tsx
+++ src/MovieTile.tsx
@@ -1,9 +1,8 @@
 import { MoviesInterface } from "./App";
-const ImagesApi = "https://image.tmdb.org/t/p/w1280";
+const ImagesApi = "https://image.tmdb.org/t/p/w200";
 export default function MovieTile({ title, poster_path, overview, vote_average }: MoviesInterface) {
         return(
             <>
-                <p className="movietile" >{title}</p>
                 <img src={ImagesApi + poster_path} alt={title}/>
             </>
         );
