6709d447d33db0d3f2cc591db35e9adb5923aab0
Dewi Jones <dewigwynj@gmail.com>
2024-02-20 18:57:28 +0000

    Gradient and row shadow

--- src/MovieBar.tsx
+++ src/MovieBar.tsx
@@ -4,7 +4,7 @@ import { MovieListInterface } from "./App";
 export default function MovieBar({title, movies}: MovieListInterface) {
     return (
         <>
-        <h2 style={{textAlign: 'left'}}>{title}</h2>
+        <h2 className="movierowTitle">{title}</h2>
         <div className="movierow">
             {movies.length > 0 && movies.map((curmovie) => <MovieTile key={curmovie.id} title={curmovie.title} poster_path ={curmovie.poster_path} overview={curmovie.overview} vote_average={curmovie.vote_average}  />)}
         </div>
--- src/MovieTile.tsx
+++ src/MovieTile.tsx
@@ -3,7 +3,7 @@ const ImagesApi = "https://image.tmdb.org/t/p/w200";
 export default function MovieTile({ title, poster_path, overview, vote_average }: MoviesInterface) {
         return(
             <>
-                <img src={ImagesApi + poster_path} alt={title}/>
+                <img className="movieTile" src={ImagesApi + poster_path} alt={title}/>
             </>
         );
 }
