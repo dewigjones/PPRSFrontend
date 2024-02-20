6f019f4914560a1a1d134a956c091c5ff5aa65f3
Dewi Jones <dewigwynj@gmail.com>
2024-02-20 11:38:04 +0000

    Left align title

--- src/MovieBar.tsx
+++ src/MovieBar.tsx
@@ -1,11 +1,12 @@
 import MovieTile from "./MovieTile";
 import { MovieListInterface } from "./App";
+
 export default function MovieBar({title, movies}: MovieListInterface) {
     return (
         <>
-        <h2>{title}</h2>
+        <h2 style={{textAlign: 'left'}}>{title}</h2>
         <div className="movierow">
-            {movies.length >0 && movies.map((curmovie) => <MovieTile title={curmovie.title} poster_path ={curmovie.poster_path} overview={curmovie.overview} vote_average={curmovie.vote_average}  />)}
+            {movies.length > 0 && movies.map((curmovie) => <MovieTile key={curmovie.id} title={curmovie.title} poster_path ={curmovie.poster_path} overview={curmovie.overview} vote_average={curmovie.vote_average}  />)}
         </div>
         </>
     );
