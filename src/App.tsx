db67c1f6580e21be0b70892769ba06c506f735d3
Dewi Jones <dewigwynj@gmail.com>
2024-02-26 21:42:42 +0000

    Filesystem investigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import SEAL from 'node-seal'
 import movieList from '../movieList.txt'
 import './App.css'
 import MovieBar from './MovieBar'
+import fs from 'fs'
 
 export interface MoviesInterface {
   id?: number;
@@ -23,7 +24,7 @@ const initSeal = () => {
   const securityLevel = seal.SecurityLevel.tc128;
   const polyModulusDegree = 16384;
   const bitSizes = [36, 36, 37];
-  const bitSize = 20; 
+  const bitSize = 20;
 
   const encParms = seal.EncryptionParameters(schemeType)
 
@@ -55,7 +56,9 @@ const initSeal = () => {
   // Create a new KeyGenerator (creates a new keypair internally)
   const keyGenerator = seal.KeyGenerator(context)
 
-  const secretKey = keyGenerator.secretKey()
+  const secretKey = keyGenerator.secretKey();
+  // secretKey.load(context, fs.readFileSync('hello.txt', 'utf8'));
+  console.log(fs);
   const publicKey = keyGenerator.createPublicKey()
   const relinKey = keyGenerator.createRelinKeys()
 
@@ -64,51 +67,53 @@ const initSeal = () => {
   const publicBase64Key = publicKey.save()
 
   console.log(secretBase64Key);
-  
+
 }
 
 function App() {
-const apikey = import.meta.env.VITE_REACT_APP_TMDB_API_TOKEN;
-const FeaturedApi = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apikey}&page=1`;
-const numOfFilmsToDisplay = 15;
+  const apikey = import.meta.env.VITE_REACT_APP_TMDB_API_TOKEN;
+  const FeaturedApi = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apikey}&page=1`;
+  const numOfFilmsToDisplay = 15;
 
-const [loading, setLoading] = useState<boolean>(true);
-const [movies, setMovies] = useState<MoviesInterface[]>([]);
-const [sealInitialised, setSealInitialised] = useState<boolean>(false);
-const [moviesFetched, setMoviesFetched] = useState<Set<number>>(new Set<number>);
-const [_, forceUpdate] = useReducer((x: number) => x + 1, 0);
-const [idMap, setIdMap] = useState<Map<number, string>>(new Map<number, string>());
-const [idMapLoaded, setIdMapLoaded] = useState<boolean>(false);
-let decryptedMovies: [number, number][] =  ([
-      [1, 2],
-      [2, 5],
-      [4, 3],
-      [3, 5],
-      [10, 3],
-      [41, 2],
-      [54, 2.2],
-      [22, 3],
-      [11, 2],
-      [56, 5],
-      [69, 2.5],
-      [45, 4.7],
-      [25, 2.7],
-      [15, 1.7],
-      [38, 3.6],
-      [23, 2.4],
-      [24, 3.2],
-      [27, 4.2],
-      
-])
-fetch(movieList).then(input => input.text()).then(text => text.split("\n").map(text => {
-  const entry = text.split("|", 2);
-  setIdMap((idMap) => idMap.set(parseInt(entry[0]), entry[1]));
-  setIdMapLoaded(true);
-}));
-const processMovies = (decryptedMovies:[number, number][], idMap: Map<number, string>, apikey: string) => {
-    decryptedMovies.sort(([,b], [, y]) => y-b);
-    const topMovies = decryptedMovies.slice(0,numOfFilmsToDisplay);
-    topMovies.forEach(([index, ]) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [movies, setMovies] = useState<MoviesInterface[]>([]);
+  const [sealInitialised, setSealInitialised] = useState<boolean>(false);
+  const [moviesFetched, setMoviesFetched] = useState<Set<number>>(new Set<number>);
+  const [_, forceUpdate] = useReducer((x: number) => x + 1, 0);
+  const [idMap, setIdMap] = useState<Map<number, string>>(new Map<number, string>());
+  const [idMapLoaded, setIdMapLoaded] = useState<boolean>(false);
+  let decryptedMovies: [number, number][] = ([
+    [1, 2],
+    [2, 5],
+    [4, 3],
+    [3, 5],
+    [10, 3],
+    [41, 2],
+    [54, 2.2],
+    [22, 3],
+    [11, 2],
+    [56, 5],
+    [69, 2.5],
+    [45, 4.7],
+    [25, 2.7],
+    [15, 1.7],
+    [38, 3.6],
+    [23, 2.4],
+    [24, 3.2],
+    [27, 4.2],
+
+  ])
+
+  fetch(movieList).then(input => input.text()).then(text => text.split("\n").map(text => {
+    const entry = text.split("|", 2);
+    setIdMap((idMap) => idMap.set(parseInt(entry[0]), entry[1]));
+    setIdMapLoaded(true);
+  }));
+
+  const processMovies = (decryptedMovies: [number, number][], idMap: Map<number, string>, apikey: string) => {
+    decryptedMovies.sort(([, b], [, y]) => y - b);
+    const topMovies = decryptedMovies.slice(0, numOfFilmsToDisplay);
+    topMovies.forEach(([index,]) => {
       setMoviesFetched(moviesFetched.add(index));
       const movieTitle = idMap.get(index);
       const MovieLookUpApi = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1`;
@@ -121,17 +126,17 @@ const processMovies = (decryptedMovies:[number, number][], idMap: Map<number, st
       };
       fetch(MovieLookUpApi, options)
         .then((res) => res.json())
-        .then((data)=> {
+        .then((data) => {
           console.log(data);
           let firstMovie: MoviesInterface = data.results[0];
-          setMovies((movies)=>[...movies, firstMovie]);
+          setMovies((movies) => [...movies, firstMovie]);
           setLoading(false);
 
         })
     })
   }
 
-const getMovies = (API: string) => {
+  const getMovies = (API: string) => {
     setLoading(true);
     fetch(API)
       .then((res) => res.json())
@@ -141,17 +146,17 @@ const getMovies = (API: string) => {
       });
   };
   useEffect(() => {
-    if(idMapLoaded && moviesFetched.size < Math.min(decryptedMovies.length, numOfFilmsToDisplay)) processMovies(decryptedMovies, idMap, apikey)
+    if (idMapLoaded && moviesFetched.size < Math.min(decryptedMovies.length, numOfFilmsToDisplay)) processMovies(decryptedMovies, idMap, apikey)
   }, [decryptedMovies, idMap, apikey]);
 
- if (!sealInitialised) {
-  initSeal();
-  setSealInitialised(true);
- }
+  if (!sealInitialised) {
+    initSeal();
+    setSealInitialised(true);
+  }
   return (
     <>
       <h1 className="pprsTitle">Privacy Preserving Recommender System</h1>
-      {loading ?(<h2>Loading</h2>)  : (<> <MovieBar title={"Movie results"} movies={movies}/> <MovieBar title={"Movie results"} movies={movies}/> <MovieBar title={"Movie results"} movies={movies}/> <MovieBar title={"Movie results"} movies={movies}/></>)}
+      {loading ? (<h2>Loading</h2>) : (<> <MovieBar title={"Movie results"} movies={movies} /> <MovieBar title={"Movie results"} movies={movies} /> <MovieBar title={"Movie results"} movies={movies} /> <MovieBar title={"Movie results"} movies={movies} /></>)}
     </>
   )
 }
